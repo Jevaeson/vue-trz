e3b6e174f009a2865affef2624d128d97321e19c
Jevaeson <zunyue1995@JevaesondeMacBook-Pro.local>
2019-01-26 22:43:18 +0800

    hello

---
+++ src/components/Button.vue
@@ -0,0 +1,16 @@
+<template>
+  <button>按钮</button>
+</template>
+
+<script>
+  // 使用 typescript 写 vue
+  export default {
+    name: "btn",
+    props: ['txt'],
+    mounted() {
+      console.log(this.txt);
+    },
+  };
+</script>
+
+
